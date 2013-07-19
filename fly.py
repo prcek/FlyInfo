3b841058f0a91886e651776ea684e5f2c643b9b3
Tomas Hluchan <tomas.hluchan@gmail.com>
2013-07-19 15:39:45 +0200

    test change

--- fly.py
+++ fly.py
@@ -5,7 +5,7 @@ class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        self.response.write('Hello, World!!')
 
 
 application = webapp2.WSGIApplication([
